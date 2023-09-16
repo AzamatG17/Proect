79c00730b13ecab10c94fb20d8cb88cae4191c6c
AzamatG17 <128783946+AzamatG17@users.noreply.github.com>
2023-09-16 13:15:31 +0500

    first commit

---
+++ Proect 1/Proect 1/Models/Category.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Proect_1.Models
+{
+    internal class Category
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+
+        public Category()
+        {
+        }
+
+        public Category(string name)
+        {
+            Name = name;
+        }
+
+        public Category(int id, string name)
+        {
+            Id = id;
+            Name = name;
+        }
+
+        public override string ToString() => $"Id: {Id}, Name: {Name}";
+    }
+}
---
+++ Proect 1/Proect 1/Models/Income.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Proect_1.Models
+{
+    internal class Income
+    {
+        public int Id { get; set; }
+        public string Description { get; set; }
+        public decimal Amount { get; set; }
+        public DateTime Date { get; set; }
+        public int CategoryId { get; set; }
+
+        public Income()
+        {
+        }
+
+        public Income(string description, decimal amount, DateTime date, int categoryId)
+        {
+            Description = description;
+            Amount = amount;
+            Date = date;
+            CategoryId = categoryId;
+        }
+
+        public Income(int id, string description, decimal amount, DateTime date, int categoryId)
+        {
+            Id = id;
+            Description = description;
+            Amount = amount;
+            Date = date;
+            CategoryId = categoryId;
+        }
+
+        public override string ToString() => $"Id: {Id}, Description: {Description}, Amount: {Amount:C}, Date: {Date}, CategoryId: {CategoryId}";
+    }
+}
